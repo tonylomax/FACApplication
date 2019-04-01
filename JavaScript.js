9149cfb91257d944e9de4459353eb7f76e946970
Anthony Lomax <44951051+tonylomax@users.noreply.github.com>
2019-04-01 22:11:53 +0100

    Update JavaScript.js
    Refactored right and left navigation functions.

--- JavaScript.js
+++ JavaScript.js
@@ -1,4 +1,8 @@
 let countNum = 1
+
+let imgArr = Array.from(document.getElementsByTagName("img"))
+
+
 let imgOne = document.getElementById("img1");
 let imgTwo = document.getElementById("img2");
 let imgThree = document.getElementById("img3");
@@ -13,54 +17,40 @@ document.addEventListener("keydown", (e) => {
   else if (e.keyCode == 37) {
     left()
   }
-  
 })
 
-function right(countnum) {
-    if (countNum == 1) {
-    countNum = 2
-    imgOne.style.display = "none"
-    imgTwo.style.display = "block"
-    imgThree.style.display = "none"
-    
+
+
+function newRight(count)  {
+  count = countNum
+//  console.log(count) //right
+  if (count < imgArr.length) {
+    imgArr.forEach((e) => {e.style.display="none"})
+    imgArr[countNum].style.display = "block"
+  } //final image
+  else if (count == imgArr.length) {
+    imgArr.forEach((e) => {e.style.display="none"})
+    imgArr[0].style.display = "block"
+    countNum = 0
   }
-  else if (countNum == 2) {
-    countNum = 3;
-    imgOne.style.display = "none"
-    imgTwo.style.display = "none"
-    imgThree.style.display = "block"
-    
-  }
-  else if (countNum == 3) {
-     countNum = 1;
-    imgOne.style.display = "block"
-    imgTwo.style.display = "none"
-    imgThree.style.display = "none"
-  }
+ countNum ++
 }
- 
-function left(countnum) {
-  if (countNum == 1) {
-    countNum = 3
-    imgOne.style.display = "none"
-    imgTwo.style.display = "none"
-    imgThree.style.display = "block"
-    
+
+function newLeft(count) {
+    count = countNum
+  if (count < imgArr.length) {
+    imgArr.forEach((e) => {e.style.display="none"})
+    imgArr[imgArr.length-count].style.display = "block" 
+} 
+  else if (count == imgArr.length) {
+    imgArr.forEach((e) => {e.style.display="none"})
+    imgArr[0].style.display = "block"
+    countNum = 0
   }
-  else if (countNum == 3) {
-    countNum = 2;
-    imgOne.style.display = "none"
-    imgTwo.style.display = "block"
-    imgThree.style.display = "none"
-    
-  }
-  else if (countNum == 2) {
-     countNum = 1;
-    imgOne.style.display = "block"
-    imgTwo.style.display = "none"
-    imgThree.style.display = "none"
-  }  
+  countNum++
 }
+
+
 
   
 play.addEventListener("click", () => {
@@ -75,19 +65,30 @@ pause.addEventListener("click", () => {
 
 
 
+let autoPlayVar = null
+
 function autoPlay() {
-  const autoPlayVar = setInterval(right, 3000)
+  if (autoPlayVar) {
+    clearInterval(autoPlayVar);
+    autoPlayVar = null;
+    console.log(autoPlayVar == false)
+  } else {
+    autoPlayVar = setInterval(right, 3000)
+    console.log(autoPlayVar == false)
+  }
+  return false;
 }
 
 
 
-play.addEventListener("click", autoPlay),
-pause.addEventListener("click", clearInterval(autoPlayVar)),  
+
+play.addEventListener("click", autoPlay)
+pause.addEventListener("click", autoPlay)
 
 
 
-document.getElementById("right").addEventListener("click", right)
+document.getElementById("right").addEventListener("click", newRight)
 
-document.getElementById("left").addEventListener("click", left)
+document.getElementById("left").addEventListener("click", newLeft)
 
 

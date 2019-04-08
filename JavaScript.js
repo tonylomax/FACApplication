54e61752b6489172fa70aaa6f3eaaf58763dfe6b
Anthony Lomax <44951051+tonylomax@users.noreply.github.com>
2019-04-08 21:27:04 +0100

    Update JavaScript.js
    Refactored code. Fixed showSlide function and simplified.

--- JavaScript.js
+++ JavaScript.js
@@ -1,58 +1,32 @@
-let countNum = 1
+let countNum = 0
 
 let imgArr = Array.from(document.getElementsByTagName("img"))
-
-
-let imgOne = document.getElementById("img1");
-let imgTwo = document.getElementById("img2");
-let imgThree = document.getElementById("img3");
-
 let play =  document.getElementById("play")
 let pause =  document.getElementById("pause")
 
+
+
 document.addEventListener("keydown", (e) => {
   if (e.keyCode == 39) {
-    right()
+    showSlide("right")
   }
   else if (e.keyCode == 37) {
-    left()
+    showSlide("left")
   }
 })
 
+imgArr.forEach((img) => {img.style.display="none"})
+imgArr[0].style.display="block"
 
-
-function newRight(count)  {
-  count = countNum
-//  console.log(count) //right
-  if (count < imgArr.length) {
-    imgArr.forEach((e) => {e.style.display="none"})
-    imgArr[countNum].style.display = "block"
-  } //final image
-  else if (count == imgArr.length) {
-    imgArr.forEach((e) => {e.style.display="none"})
-    imgArr[0].style.display = "block"
-    countNum = 0
-  }
- countNum ++
+function showSlide(e) { 
+  imgArr.forEach((img) => {img.style.display="none"})
+  e == "right" ? countNum ++ :false
+  e == "left" ? countNum --:false
+  countNum < 0 ? countNum = imgArr.length-1:false
+  countNum == imgArr.length ? countNum = 0:false
+  imgArr[countNum].style.display = "block"
 }
-
-function newLeft(count) {
-    count = countNum
-  if (count < imgArr.length) {
-    imgArr.forEach((e) => {e.style.display="none"})
-    imgArr[imgArr.length-count].style.display = "block" 
-} 
-  else if (count == imgArr.length) {
-    imgArr.forEach((e) => {e.style.display="none"})
-    imgArr[0].style.display = "block"
-    countNum = 0
-  }
-  countNum++
-}
-
-
-
-  
+   
 play.addEventListener("click", () => {
   pause.style.display = "initial" 
   play.style.display = "none" 
@@ -71,24 +45,20 @@ function autoPlay() {
   if (autoPlayVar) {
     clearInterval(autoPlayVar);
     autoPlayVar = null;
-    console.log(autoPlayVar == false)
   } else {
-    autoPlayVar = setInterval(right, 3000)
-    console.log(autoPlayVar == false)
+    autoPlayVar = setInterval(() => {showSlide("right")}, 3000)
   }
   return false;
 }
 
 
-
-
 play.addEventListener("click", autoPlay)
 pause.addEventListener("click", autoPlay)
+                          
 
+document.getElementById("right").addEventListener("click", () => { showSlide("right"); })
 
+document.getElementById("left").addEventListener("click", () => { showSlide("left"); })
 
-document.getElementById("right").addEventListener("click", newRight)
-
-document.getElementById("left").addEventListener("click", newLeft)
 
 

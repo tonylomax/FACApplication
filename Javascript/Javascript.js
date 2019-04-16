cca87df937033a22a79f9c86ccab00613fca2fb6
Anthony Lomax <44951051+tonylomax@users.noreply.github.com>
2019-04-16 20:41:52 +0100

    Update Javascript.js
    Added comments

--- Javascript/Javascript.js
+++ Javascript/Javascript.js
@@ -1,12 +1,13 @@
 
 let countNum = 0
 
+/* Place all carousel images into array */
 let imgArr = Array.from(document.getElementsByTagName("img"))
+
 let play =  document.getElementById("play")
 let pause =  document.getElementById("pause")
 
-
-
+/* Move carousel on keypress */
 document.addEventListener("keydown", (e) => {
   if (e.keyCode == 39) {
     showSlide("right")
@@ -16,9 +17,13 @@ document.addEventListener("keydown", (e) => {
   }
 })
 
+/* Make all carousel images hidden...*/
 imgArr.forEach((img) => {img.style.display="none"})
+/* ... except the very first image*/
 imgArr[0].style.display="block"
 
+/* Funcation that takes the nav arrow direction as an argument. Right increases 
+the counter and displayes the next image along, left displays the previous image*/
 function showSlide(e) { 
   imgArr.forEach((img) => {img.style.display="none"})
   e == "right" ? countNum ++ :false
@@ -27,7 +32,8 @@ function showSlide(e) {
   countNum == imgArr.length ? countNum = 0:false
   imgArr[countNum].style.display = "block"
 }
-   
+
+/* Change display of play/pause buttons when clicked */
 play.addEventListener("click", () => {
   pause.style.display = "initial" 
   play.style.display = "none" 
@@ -37,6 +43,8 @@ pause.addEventListener("click", () => {
   play.style.display = "initial" 
   pause.style.display = "none" 
 })
+
+/* Auto play function*/
 
 let autoPlayVar = null
 
@@ -50,10 +58,9 @@ function autoPlay() {
   return false;
 }
 
+/* Event listeners for play/pause and nav buttons*/
 play.addEventListener("click", autoPlay)
 pause.addEventListener("click", autoPlay)
-                          
-
 document.getElementById("right").addEventListener("click", () => { showSlide("right"); })
 document.getElementById("left").addEventListener("click", () => { showSlide("left"); })
 
